697739e90b1a4eb947885a31ecc83567f1b81501
Jack Choi <pitou_106@naver.com>
2019-08-11 20:52:06 +0900

    12. Applied redux

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+//Router
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+//Components
 import { App, Home, Login, Register } from 'containers'
+//Redux
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from 'reducers'
+import thunk from 'redux-thunk'
+
+const store = createStore(reducers, applyMiddleware(thunk))
 
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(
-    <Router>
-        <Route path="/" component={App}/> 
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-    </Router>, rootElement)
+    <Provider store={store}>
+        <Router>
+            <Route path="/" component={App} />
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+        </Router>
+    </Provider>
+    , rootElement)
 
 if (module.hot) {
     module.hot.accept()
