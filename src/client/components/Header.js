5c46afede65106ee97ad252b67c20d10ff30b99c
Jack Choi <pitou_106@naver.com>
2019-08-11 19:02:15 +0900

    Add Header component

---
+++ src/client/components/Header.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Header extends Component {
+    static propTypes = {
+        isLoggedIn: PropTypes.bool,
+        onLogout: PropTypes.func
+    }
+
+    static defaultProps = {
+        isLoggedIn: false,
+        onLogout: () => {
+            console.error("onLoout not defined")
+        }
+        
+    }
+    render() {
+        const loginButton = (
+            <li>
+                <a><i className="material-icons">vpn_key</i></a>
+            </li>
+        )
+        const logoutButton = (
+            <li>
+                <a><i className="material-icons">lock_open</i></a>
+            </li>
+        )
+        return (
+            <nav>
+                <div className="nav-wrapper blue darken-1">
+                    <a className="brand-logo center">Memopad</a>
+                    <ul>
+                        <li><a><i className="material-icons">search</i></a></li>
+                    </ul>
+                    <div className="right">
+                        <ul>
+                            {this.props.isLoggedIn ? logoutButton : loginButton}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header
--- src/client/containers/App.js
+++ src/client/containers/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { Header } from 'components'
 
-export default class App extends Component {
+export class App extends Component {
     render() {
         return (
-            <div>
-                Hello React with Expre
-            </div>
+            <Header/>
         )
     }
 }
+
+export default App
